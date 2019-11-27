65d356a0c9a42cae6174ebd457283545017dca83
zyb17863942950 <zyb17863942950@163.com>
2019-11-27 09:33:08 +0800

    submit

---
+++ mgj/src/main/java/com/qingao/mgj/controller/GoodsInfoController.java
@@ -0,0 +1,11 @@
+package com.qingao.mgj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/goodsinfocontroller")
+public class GoodsInfoController {
+	
+	
+}
---
+++ mgj/src/main/java/com/qingao/mgj/service/AppService.java
@@ -0,0 +1,9 @@
+package com.qingao.mgj.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppService {
+	
+	
+}
